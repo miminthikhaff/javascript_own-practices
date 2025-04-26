2b09b32cf66fb00e4bb37f2e5060783400cf0ab6
Mohamed Inthikhaff <mhdinthikaff@gmail.com>
2025-04-27 00:09:43 +0530

    Add demoFunction.js with multiple function examples and console outputs

---
+++ demoFunction.js
@@ -0,0 +1,60 @@
+function abc() {
+  console.log("Function is working");
+}
+
+// ---------------------------------------------------------------------------------------------------------------
+
+
+abc(); // Hello
+
+function myname() {
+  console.log("My name is John Doe");
+}
+
+// ---------------------------------------------------------------------------------------------------------------
+
+myname(); // My name is John Doe
+
+function samsung() {
+  console.log("This is a Samsung Phone");
+}
+
+function oppo() {
+  console.log("This is an Oppo Phone");
+}
+
+function apple() {
+  console.log("This is an Apple Phone");
+}
+
+function redmi() {
+  console.log("This is a Redmi Phone");
+}
+
+samsung();
+redmi();
+
+
+// ---------------------------------------------------------------------------------------------------------------
+
+var a = 10;
+var b = 20;
+function add() {
+  console.log(a + b); // 30
+}
+
+add(); // 30
+
+// ---------------------------------------------------------------------------------------------------------------
+
+var factor = "Kamal";
+var fplayer = "Dhoni";
+var fmovie = "Anbe Sivam";
+
+function favourite() {
+  console.log("Favorite Actor: " + factor);
+  console.log("Favorite Player: " + fplayer);
+  console.log("Favorite Movie: " + fmovie);
+}
+
+favourite();
